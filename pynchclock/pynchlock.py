c42f78c55be74de63a4e5d2e06d1b385ca8e6f47
Michael Lerch <mdlerch@gmail.com>
2014-07-28 22:21:12 -0600

    Get csv working and switch to dictionary

--- pynchclock/pynchlock.py
+++ pynchclock/pynchlock.py
@@ -3,30 +3,36 @@ import csv
 import math
 
 def printHours(hours):
+    i = 1
+    print("")
     for k,t in hours.iteritems():
         h = t / 3600.0
         m = (h - math.floor(h)) * 60
-        print("Job {0}, {1:.0f} hours, {2:.2f} minutes".format(k, math.floor(h), m))
+        print("{3}.  {0}: {1:.0f} hours, {2:.2f} minutes".format(k, math.floor(h), m, i))
+        i += 1
+    print("")
 
 def readJobs(file):
     with open(file) as jobs_file:
         jobs_reader = csv.reader(jobs_file)
+        hours = {}
         for row in jobs_reader:
-            print row
+            hours[row[0]] = float(row[1])
+        return hours
 
 
-jobs = [ 0, 1, 2, 3, 4, 5]
-hours = {x: 0.0 for x in jobs}
+hours = readJobs("/home/mike/.config/pynchclock/jobs.csv")
 
-print(jobs)
+printHours(hours)
 
-readJobs("/home/mike/.config/pynchclock/jobs.csv")
-
-cur_job = input('Pick a job (0 to quit): ')
+jobs = hours.keys()
+cur_job_i = input('Pick a job (0 to quit): ')
+cur_job = jobs[cur_job_i - 1]
 while (cur_job != 0):
     printHours(hours)
     start = time.time()
-    next_job = input('Pick a job (0 to quit): ')
+    next_job_i = input('Pick a job (0 to quit): ')
+    next_job = jobs[next_job_i]
     hours[cur_job] += time.time() - start
     cur_job = next_job
 
