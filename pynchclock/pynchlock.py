b94d5f1b65efd7366cd65becef09c506106eb9b3
Michael Lerch <mdlerch@gmail.com>
2015-01-13 16:23:31 -0700

    Show jobs in order of jobs file

--- pynchclock/pynchlock.py
+++ pynchclock/pynchlock.py
@@ -3,75 +3,73 @@ import csv
 import math
 import curses
 
-# change how things are stored:
-# one time called clock?
-# dictionary
-# current: string key of current job or integer index of current job?
-# timesheet: dictionary of job names and times
-
 def printHours(clock, stdscr, active):
     i = 0
-    for k,t in clock['timesheet'].iteritems():
+    for job in clock['order']:
+        t = clock['timesheet'][job]
         h = t / 3600.0
         m = (h - math.floor(h)) * 60
-        jobstring = "{3}.  {0}: {1:.0f} hours, {2:.0f} minutes".format(k, math.floor(h), m, i+1)
-        if i == active:
+        if job != "None":
+            jobstring = "{3}. {0}: {1:.0f} hours, {2:.0f} minutes".format(job, math.floor(h), m, i+1)
+        else:
+            jobstring = "{1}. {0}".format(job, i+1)
+        if clock['order'][i] == active:
             stdscr.addstr(i, 0, jobstring, curses.A_REVERSE)
-        elif i == clock['cur_job']:
+        elif clock['order'][i] == clock['cur_job']:
             stdscr.addstr(i, 0, jobstring, curses.color_pair(1))
         else:
             stdscr.addstr(i, 0, jobstring)
         i += 1
-    jobstring = "{0}.  None".format(i+1)
-    if i == active:
-        stdscr.addstr(i, 0, jobstring, curses.A_REVERSE)
-    elif i == clock['cur_job']:
-        stdscr.addstr(i, 0, jobstring, curses.color_pair(1))
-    else:
-        stdscr.addstr(i, 0, jobstring)
     stdscr.refresh()
 
 def readJobs(file):
     with open(file) as jobs_file:
         jobs_reader = csv.reader(jobs_file)
         clock = { 'timesheet':{} }
+        clock['order'] = []
         for row in jobs_reader:
             (clock['timesheet'])[row[0]] = float(row[1])
-        njobs = len(clock['timesheet'].keys())
-        clock['current'] = njobs
+            clock['order'].append(row[0])
+        clock['timesheet']['None'] = 0.0
+        clock['cur_job'] = "None"
+        clock['order'].append("None")
         return clock
 
 def writeJobs(file, clock):
     with open(file, "wb") as jobs_file:
         jobs_writer = csv.writer(jobs_file)
-        for key, val in clock['timesheet'].items():
-            jobs_writer.writerow([key, val])
+        for job in clock['order']:
+            if job != "None":
+                time = clock['timesheet'][job]
+                jobs_writer.writerow([job, time])
 
 def updateTimes(clock, start):
-    if clock['cur_job'] != njobs:
-        job = clock['timesheet'].keys()[clock['cur_job']]
-        clock['timesheet'][job] += time.time() - start
+    if clock['cur_job'] != "None":
+        clock['timesheet'][clock['cur_job']] += time.time() - start
 
 
 def selectJob(clock, stdscr, jobfile, start):
     active = clock['cur_job']
+    i = clock['order'].index(active)
     while(1):
         printHours(clock, stdscr, active)
         njobs = len(clock['timesheet'].keys())
         c = stdscr.getch()
 
         if c == curses.KEY_UP or (c < 256 and chr(c) == 'k'):
-            if active > 0:
-                active = active - 1
+            if clock['order'].index(active) > 0:
+                i = i - 1
+                active = clock['order'][i]
         elif c == curses.KEY_DOWN or (c < 256 and chr(c) == 'j'):
-            if active < njobs:
-                active = active + 1
+            if clock['order'].index(active) < njobs - 1:
+                i = i + 1
+                active = clock['order'][i]
         elif c == curses.KEY_ENTER or (c < 256 and chr(c) == "\n"):
             updateTimes(clock, start)
             break
         elif c == ord('p'):
             updateTimes(clock, start)
-            active = njobs
+            active = "None"
             break
         elif c == ord('q'):
             updateTimes(clock, start)
@@ -113,7 +111,7 @@ curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
 
 jobs = clock['timesheet'].keys()
 njobs = len(jobs)
-clock['cur_job'] = njobs
+clock['cur_job'] = "None"
 
 selectJob(clock, stdscr, jobfile, time.time())
 
