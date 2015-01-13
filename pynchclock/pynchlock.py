0895a83e4bcac6561cb62929ca8a5041f688dda8
Michael Lerch <mdlerch@gmail.com>
2015-01-13 15:12:46 -0700

    Move to one big dictionary

--- pynchclock/pynchlock.py
+++ pynchclock/pynchlock.py
@@ -9,15 +9,15 @@ import curses
 # current: string key of current job or integer index of current job?
 # timesheet: dictionary of job names and times
 
-def printHours(hours, stdscr, cur_job, active):
+def printHours(clock, stdscr, active):
     i = 0
-    for k,t in hours.iteritems():
+    for k,t in clock['timesheet'].iteritems():
         h = t / 3600.0
         m = (h - math.floor(h)) * 60
         jobstring = "{3}.  {0}: {1:.0f} hours, {2:.0f} minutes".format(k, math.floor(h), m, i+1)
         if i == active:
             stdscr.addstr(i, 0, jobstring, curses.A_REVERSE)
-        elif i == cur_job:
+        elif i == clock['cur_job']:
             stdscr.addstr(i, 0, jobstring, curses.color_pair(1))
         else:
             stdscr.addstr(i, 0, jobstring)
@@ -25,7 +25,7 @@ def printHours(hours, stdscr, cur_job, active):
     jobstring = "{0}.  None".format(i+1)
     if i == active:
         stdscr.addstr(i, 0, jobstring, curses.A_REVERSE)
-    elif i == cur_job:
+    elif i == clock['cur_job']:
         stdscr.addstr(i, 0, jobstring, curses.color_pair(1))
     else:
         stdscr.addstr(i, 0, jobstring)
@@ -34,43 +34,57 @@ def printHours(hours, stdscr, cur_job, active):
 def readJobs(file):
     with open(file) as jobs_file:
         jobs_reader = csv.reader(jobs_file)
-        hours = {}
+        clock = { 'timesheet':{} }
         for row in jobs_reader:
-            hours[row[0]] = float(row[1])
-        return hours
+            (clock['timesheet'])[row[0]] = float(row[1])
+        njobs = len(clock['timesheet'].keys())
+        clock['current'] = njobs
+        return clock
 
-def writeJobs(file, hours):
+def writeJobs(file, clock):
     with open(file, "wb") as jobs_file:
         jobs_writer = csv.writer(jobs_file)
-        for key, val in hours.items():
+        for key, val in clock['timesheet'].items():
             jobs_writer.writerow([key, val])
 
-def selectJob(hours, stdscr, njobs, cur_job, jobfile, start):
-    active = cur_job
+def updateTimes(clock, start):
+    if clock['cur_job'] != njobs:
+        job = clock['timesheet'].keys()[clock['cur_job']]
+        clock['timesheet'][job] += time.time() - start
+
+
+def selectJob(clock, stdscr, jobfile, start):
+    active = clock['cur_job']
     while(1):
-        printHours(hours, stdscr, cur_job, active)
+        printHours(clock, stdscr, active)
+        njobs = len(clock['timesheet'].keys())
         c = stdscr.getch()
-        if c == ord('q'):
-            if cur_job != njobs:
-                hours[hours.keys()[cur_job]] += time.time() - start
-            writeJobs(jobfile, hours)
-            curses.nocbreak(); stdscr.keypad(0); curses.echo(); curses.endwin()
-            exit()
-        if c == ord('p'):
-            active = njobs
-            break
-        elif c == curses.KEY_UP or (c < 256 and chr(c) == 'k'):
+
+        if c == curses.KEY_UP or (c < 256 and chr(c) == 'k'):
             if active > 0:
                 active = active - 1
         elif c == curses.KEY_DOWN or (c < 256 and chr(c) == 'j'):
             if active < njobs:
                 active = active + 1
         elif c == curses.KEY_ENTER or (c < 256 and chr(c) == "\n"):
+            updateTimes(clock, start)
             break
-    return active
+        elif c == ord('p'):
+            updateTimes(clock, start)
+            active = njobs
+            break
+        elif c == ord('q'):
+            updateTimes(clock, start)
+            writeJobs(jobfile, clock)
+            curses.nocbreak(); stdscr.keypad(0); curses.echo(); curses.endwin()
+            exit()
+        elif c == ord('a'):
+
+
+    clock['cur_job'] = active
 
 jobfile = "/home/mike/.config/pynchclock/jobs.csv"
-hours = readJobs(jobfile)
+clock = readJobs(jobfile)
 
 # initialize curses
 stdscr = curses.initscr(); stdscr.keypad(1); curses.start_color()
@@ -78,16 +92,13 @@ stdscr.refresh(); curses.noecho(); curses.curs_set(0)
 winy, winx = stdscr.getmaxyx()
 curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
 
-jobs = hours.keys()
+jobs = clock['timesheet'].keys()
 njobs = len(jobs)
-cur_job = njobs
+clock['cur_job'] = njobs
 
-cur_job = selectJob(hours, stdscr, njobs, cur_job, jobfile, time.time())
+selectJob(clock, stdscr, jobfile, time.time())
 
 while (1):
     start = time.time()
 
-    next_job = selectJob(hours, stdscr, njobs, cur_job, jobfile, start)
-    if cur_job != njobs:
-        hours[jobs[cur_job]] += time.time() - start
-    cur_job = next_job
+    selectJob(clock, stdscr, jobfile, start)
