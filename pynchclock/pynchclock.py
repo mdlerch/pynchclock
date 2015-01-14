dc922a9597f4b27b14578a97591e5e4beb516a18
Michael Lerch <mdlerch@gmail.com>
2015-01-13 20:02:26 -0700

    Fix up bugs

--- pynchclock/pynchclock.py
+++ pynchclock/pynchclock.py
@@ -9,10 +9,12 @@ def printHours(clock, stdscr, active):
         t = clock['timesheet'][job]
         h = t / 3600.0
         m = (h - math.floor(h)) * 60
+
         if job != "None":
             jobstring = "{3}. {0}: {1:.0f} hours, {2:.0f} minutes".format(job, math.floor(h), m, i+1)
         else:
             jobstring = "{1}. {0}".format(job, i+1)
+
         if clock['order'][i] == active:
             stdscr.addstr(i, 0, jobstring, curses.A_REVERSE)
         elif clock['order'][i] == clock['cur_job']:
@@ -48,10 +50,11 @@ def updateTimes(clock, start):
         clock['timesheet'][clock['cur_job']] += time.time() - start
 
 
-def selectJob(clock, stdscr, jobfile, start):
+def eventLoop(clock, stdscr, jobfile):
+    start = None
     active = clock['cur_job']
-    i = clock['order'].index(active)
     while(1):
+        i = clock['order'].index(active)
         printHours(clock, stdscr, active)
         njobs = len(clock['timesheet'].keys())
         c = stdscr.getch()
@@ -60,17 +63,22 @@ def selectJob(clock, stdscr, jobfile, start):
             if clock['order'].index(active) > 0:
                 i = i - 1
                 active = clock['order'][i]
+            stdscr.clear()
         elif c == curses.KEY_DOWN or (c < 256 and chr(c) == 'j'):
             if clock['order'].index(active) < njobs - 1:
                 i = i + 1
                 active = clock['order'][i]
+            stdscr.clear()
         elif c == curses.KEY_ENTER or (c < 256 and chr(c) == "\n"):
             updateTimes(clock, start)
-            break
+            clock['cur_job'] = active
+            start = time.time()
+            stdscr.clear()
         elif c == ord('p'):
             updateTimes(clock, start)
             active = "None"
-            break
+            clock['cur_job'] = active
+            stdscr.clear()
         elif c == ord('q'):
             updateTimes(clock, start)
             writeJobs(jobfile, clock)
@@ -81,24 +89,32 @@ def selectJob(clock, stdscr, jobfile, start):
             stdscr.addstr(maxy-1, 0, "New job: ")
             curses.echo()
             curses.curs_set(1)
-            newjob = stdscr.getstr(maxy-1, 10, 30)
+            newjob = stdscr.getstr(maxy-1, 9, 30)
             clock['timesheet'][newjob] = 0.0
+            clock['order'].remove("None")
+            clock['order'].append(newjob)
+            clock['order'].append("None")
             curses.noecho()
             curses.curs_set(0)
             stdscr.clear()
+            active = newjob
         elif c == ord('d'):
             maxy, maxx = stdscr.getmaxyx()
+            if active == "None":
+                stdscr.addstr(maxy-1, 0, "Cannot delete None")
+
             stdscr.addstr(maxy-1, 0, "Are you sure you wish to delete[y/n]? ")
             curses.echo()
             curses.curs_set(1)
-            c = stdscr.getch(maxy-1, 36)
+            c = stdscr.getch(maxy-1, 38)
             if c == ord('y'):
-                clock['timesheet'].pop(clock['timesheet'].keys()[active])
+                clock['timesheet'].pop(active)
+                clock['order'].remove(active)
             curses.noecho()
             curses.curs_set(0)
             stdscr.clear()
+            active = clock['order'][i]
 
-    clock['cur_job'] = active
 
 def main():
     jobfile = "/home/mike/.config/pynchclock/jobs.csv"
@@ -110,13 +126,6 @@ def main():
     winy, winx = stdscr.getmaxyx()
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
 
-    jobs = clock['timesheet'].keys()
-    njobs = len(jobs)
     clock['cur_job'] = "None"
 
-    selectJob(clock, stdscr, jobfile, time.time())
-
-    while (1):
-        start = time.time()
-
-        selectJob(clock, stdscr, jobfile, start)
+    eventLoop(clock, stdscr, jobfile)
